e85139e3b293148587da90a88ef2fbd1638d7077
unknown <brahim.bouargane2000@gmail.com>
2022-05-09 03:30:03 +0100

    first commit for test

---
+++ src/components/Dashboard.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react"
+import { Card, Button, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
+
+export default function Dashboard() {
+  const [error, setError] = useState("")
+  const { currentUser, logout } = useAuth()
+  const history = useHistory()
+
+  async function handleLogout() {
+    setError("")
+
+    try {
+      await logout()
+      history.push("/login")
+      window.location.reload(true)
+    } catch {
+      setError("Failed to log out")
+    }
+  }
+  function handlUpdate() {
+    setError("")
+
+    try {
+      
+      history.push("/update-profile")
+      window.location.reload(true)
+    } catch {
+      setError("Failed ")
+    }
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email:</strong> {currentUser.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3" onClick={handlUpdate} >
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </>
+  )
+}
